bf49885a5fe5a70f294bd477278a99cf58fb7f0f
FacTrónica <webmaster@factronica.cl>
2016-09-05 00:29:59 -0300

    Update FormatoFacturaElectronica.php

--- FormatoFacturaElectronica.php
+++ FormatoFacturaElectronica.php
@@ -261,7 +261,27 @@ $detalle["1"]["DescuentoPct"]="0";
 $detalle["1"]["DscItemPesos"]="0";
 $detalle["1"]["IndExe"]="0";
 
-
+##################################################################                                                
+#######    REFERENCIAS DE FACTURACIÓN CONTRA NOTA DE PEDIDO/VENTA                                                                              
+##################################################################
+#
+# Debe ir vacio
+$doc_referencia["1"]["IndGlobal"]="";    
+#
+# Folio de la Nota de Pedido o Nota de venta
+$doc_referencia["1"]["FolioRef"]="3345";                                                  
+#
+# 802 es el codigo para notas de pedido/venta
+$doc_referencia["1"]["TpoDocRef"]="802"; 
+#
+# Comentario de la Referencia
+$doc_referencia["1"]["RazonRef"]="Nota de Pedido 3345";                                                           
+#
+# El codigo de referencia debe ir vacio
+$doc_referencia["1"]["CodRef"]="";
+#
+# Fecha de de emisión de la nota de pedido formato aaaa-mm-dd
+$doc_referencia["1"]["FchRef"]="2016-04-28";   
  
 ##############################################################                                                 
 #######   DATOS CERTIFICADO DE PERSONA QUE FIRMA Y ENVIA                                                       
